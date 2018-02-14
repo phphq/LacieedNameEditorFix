dab68c0cb21523b01d60de577e8a4d691523e0bb
Scott Lucht <scott@phphq.net>
2018-02-14 14:38:02 -0600

    Formatting again...

--- LacieedNameEditorFix/Classes/Common.cs
+++ LacieedNameEditorFix/Classes/Common.cs
@@ -1,11 +1,14 @@
-﻿
+﻿using System.Diagnostics;
+
 namespace LacieedNameEditorFix.Classes
 {
     public class Common
     {
         public static void GoToNovaHq()
         {
-            System.Diagnostics.Process.Start(Config.NoavHqUrlApp);
+            Process.Start(Config.NoavHqUrlApp);
         }
+
     }
+
 }
